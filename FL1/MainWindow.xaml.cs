a1b51e8b87bb00ac1aceff0fa2d2ef6d6cdaa818
Erik Öberg <eriobe@miun.se>
2024-11-04 10:36:51 +0100

    Add project files.

---
+++ FL1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FL1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            // Här kommer koden för knappen att köras
+            //MessageBox.Show("Hej Erik");
+
+            // hur kan vi byta ut Erik?
+
+            // datatyper
+
+            // låt oss skapa en variabel
+
+            // en variabel är en låda som kan innehålla och spara värden
+
+            // vi skapar en variabel med namnet!
+            // mönster för variabler
+            // först: datatyp
+            // därefter: namnet på variabeln
+            // eventuellt: startvärdet för variabeln
+
+            // https://www.w3schools.com/cs/cs_data_types.php
+            string name;
+
+            // vi ska tilldela värde till vår variabel
+            // det gör vi med ett enkelt =
+
+            // detta gör vi från höger till vänster
+            name = "Erik";
+            name = "Yasmine";
+            //MessageBox.Show(name);
+
+            // Vi kan inte enbart tilldela värden till variabler
+            // de kan också användas för attribut på kontroller
+            // alla attribut når vi med punkt-notation
+            btnOk.Content = name;
+        }
+    }
+}
