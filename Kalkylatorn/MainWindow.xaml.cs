28ed4c4c025442e05f84d044483aac021864f15b
Erik Öberg <eriobe@miun.se>
2024-11-06 11:37:30 +0100

    FL 3 - fixade kod för miniräknaren - la till yatzy

--- Kalkylatorn/MainWindow.xaml.cs
+++ Kalkylatorn/MainWindow.xaml.cs
@@ -16,10 +16,65 @@ namespace Kalkylatorn
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        int _counter;
+        public MainWindow() // konstruktor
         {
             InitializeComponent();
+            _counter = 100;
         }
 
+        private void OnButtonAdd(object sender, RoutedEventArgs e)
+        {
+            // hämta alla värden från gränssnittet så att jag kan beräkna
+            // saker i C# kallas objekt
+            // OO objektorienterad programmering
+            // objekt har egenskaper
+            // objekt har beteenden = metoder = har alltid parentes
+            // ByeIceCream("Cornetto vanilj");
+
+
+            double firstValue = double.Parse(txtFirstValue.Text);
+            double secondValue = int.Parse(txtSecondValue.Text);
+            double result = 0;
+        
+
+            // varför sätter jag denna till 0?
+            result = 0;
+          
+            // gör beräkning
+            result = firstValue + secondValue;  
+            txtResult.Text=result.ToString(); // presenterar värdet som en sträng
+            // glapp med massa kodrader
+            MessageBox.Show(result.ToString());
+            // txtResult.Text = (firstValue + secondValue).ToString();
+            _counter++;
+        }
+
+        private void OnButtonDivision(object sender, RoutedEventArgs e)
+        {
+            double result =0.0;
+            double firstValue = double.Parse(txtFirstValue.Text);
+            double secondValue = double.Parse(txtSecondValue.Text);
+
+            result = firstValue / secondValue;
+
+            txtResult.Text = Math.Round(result, 2).ToString();
+
+
+            //// mer om division
+            //int divisor = 3;
+            //result = firstValue / divisor;
+            //// nu castar jag min variabel till en integer
+            //int result2 = (int)firstValue / divisor;
+            //txtResult.Text = result2.ToString(); // presenterar värdet som en sträng
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            
+            _counter--; // counter = counter -1;
+            txtResult.Text = _counter.ToString();
+
+        }
     }
 }
---
+++ YatzyGame/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YatzyGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonOk(object sender, RoutedEventArgs e)
+        {
+            int diceOne, diceTwo, diceThree, diceFour, diceFive;
+
+            // Det här ska vi fixa längre fram i våra föreläsningar
+            // få till en sätt att kasta våra tärningar och de dem värden
+
+            // eftersom vi inte kan kasta tärningar behöver vi testdata
+            diceOne = 2;
+            diceTwo = 1;
+            diceThree = 2;
+            diceFour = 3;
+            diceFive = 4;
+            
+            // summer tärningarna
+            // total 
+            // och alla ettor
+
+            // när vi har kastat tärningarna, vill vi att användare
+            // ska kunna mata in värdet hen fick vid kastet i rätt ruta
+        }
+    }
+}
