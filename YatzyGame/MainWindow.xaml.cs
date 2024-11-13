e2b1f03131e0c191977f43a3e38171d81c39e524
Erik Öberg <erik.oberg@miun.se>
2024-11-13 11:42:57 +0100

    Föreläsning där vi har tittat på hur vi kan med en loop titta på två olika arrayer
    sett till att vi kan räkna summan för en viss kategori

--- YatzyGame/MainWindow.xaml.cs
+++ YatzyGame/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace YatzyGame
 {
@@ -17,9 +8,9 @@ namespace YatzyGame
     public partial class MainWindow : Window
     {
         private int _total = 0;
-        // varför skriver jag new???
-        // jo, det för att vi gör ett objekt
-        // från en mall = klass
+        // vi skapar en array med fem fack. En för varje tärning
+        private int[] _dices = new int[5];
+
 
         // vi kallar detta även för instans
         private static readonly Random _random = new Random();
@@ -43,7 +34,7 @@ namespace YatzyGame
             // Vi måste styra koden
             // antingen är talsumman rätt, eller så är talsumman fel
             // vi kontrollerar tvåor
-            if (categoryTwos == 0 || categoryTwos == 2 || categoryTwos == 4 || categoryTwos == 6 || categoryTwos ==8 || categoryTwos == 10)
+            if (categoryTwos == 0 || categoryTwos == 2 || categoryTwos == 4 || categoryTwos == 6 || categoryTwos == 8 || categoryTwos == 10)
             {
                 _total += categoryTwos;
             }
@@ -55,7 +46,7 @@ namespace YatzyGame
             }
             _total = categoryOnes + categoryTwos + categoryThrees + categoryFours + categoryFives + categorySixes;
 
-                // får spelaren bonus??
+            // får spelaren bonus??
             if (_total >= 63)  // allt inom parentesen måste kunna svara på formen ja eller nej
             {
                 // det som ligger inom kodblocket körs BARA när villkoret valideras som sant
@@ -83,7 +74,7 @@ namespace YatzyGame
             // få till en sätt att kasta våra tärningar och de dem värden
 
             // eftersom vi inte kan kasta tärningar behöver vi testdata
-          
+
 
             // summer tärningarna
             // total 
@@ -95,86 +86,105 @@ namespace YatzyGame
 
         private void btnRollDice_Click(object sender, RoutedEventArgs e)
         {
-            int diceOne = 0, diceTwo = 0, diceThree = 0, diceFour = 0, diceFive = 0;
+            // _dices = new int[5];
+            // här kastar vi alltid alla tärningar
+            //for (int i = 0; i < _dices.Length; i++)
+            //{
+            //    _dices[i] = _random.Next(1, 7);
+            //}
 
-            // vi vill slumpa ett värde på min tärning
+            // vi vill kunna spara tärningar
+            //bool rollDiceOne = chkDiceOne.IsChecked == true;
 
-            // Vi behöver skaffa en slumpgenerator
-            // vi skapar slumpgenerator som en instansvariabel
-            // global variabel
-
-            // vi vill slumpa fram ett rimligt värde för första tärningen
-            // 1, 2, 3, 4, 5, 6
-            diceOne = _random.Next(1, 7);
-            diceTwo = _random.Next(1, 7);
-            diceThree = _random.Next(1, 7);
-            diceFour = _random.Next(1, 7);
-            diceFive = _random.Next(1, 7);
-
-            lblDiceOne.Content = diceOne.ToString();
-            lblDiceTwo.Content = diceTwo.ToString();
-            lblDiceThree.Content = diceThree.ToString();
-            lblDiceFour.Content = diceFour.ToString();
-            lblDiceFive.Content = diceFive.ToString();
-
-            // måste jag verkligen hålla på med så många variabler? Jag blir tokig
-            // av att du hela tiden måste klippa och klistra
-            // jag vill ha en array
-            // om du sätter en hakparentes efter datatypen så får du en array
-            // arrayen är en låda med många variabler
-
-            // vi skapar en array med fem fack. En för varje tärning
-            int[] dices = new int[5];
-
-            // vi måste stoppa in våra tärningsslag
-            // men vi behöver hålla koll på i vilket fack respektive
-            // tärninsslag ska sparas.
-
-            // vi använder ett indexvärde
-            // det börjar ALLTID på noll 
-
-            // tärning nummer 1, har index 0
-            //dices[0] = diceOne;
-
-            //// tärning nummer 2, har index 1
-            //dices[1] = diceTwo;
-
-            //dices[2] = diceThree;
-            //dices[3] = diceFour;
-            //dices[4] = diceFive;
+            // Gör så Erik blir smart!
+            // varför inte skapa en array till!
+            // sparade tärningar:
+            //  0      1      2     3     4
+            // [true, false, true, true, true]
+            // [2,    5,      5,    5,     6]
+            bool[] savedDices = new bool[_dices.Length];
+            savedDices[0] = chkDiceOne.IsChecked == true;
+            savedDices[1] = chkDiceTwo.IsChecked == true;
+            savedDices[2] = chkDiceThree.IsChecked == true;
+            savedDices[3] = chkDiceFour.IsChecked == true;
+            savedDices[4] = chkDiceFive.IsChecked == true;
 
 
-            // en array är som gjord för en snurra eller loop
-
-            // LOOP är superbra för att köra kod i en sekvens 
-            // en av alla loopar är for-loopen
-
-            // Syntax för en for-loop
-            // tre statements
-            // uttryck1: startvärdet. Ofta är detta 0 eftersom det
-            // lirar bra med ett index som börjar med noll
-
-            //uttryck2: Ett villkor som valideras som sant eller falskt
-            // som avgör frågan: Ska jag loopa ett varv till?
-            // i < 5
-
-            // statment 3: Hur mycket ka värdet av vår variabel (i)
-            // öka varje varv i loopen?
-            // det vanligaste här är en etta
-            for(int i = 0; i < 5; i++)
+            // Vi skulle kunna testa att göra en kontroll av en array till!
+            for (int i = 0; i < _dices.Length; i++)
             {
-                dices[i] = _random.Next(1, 7);
+                bool diceIsNotSaved = savedDices[i] == false;
+                if (diceIsNotSaved)
+                {
+                    _dices[i] = _random.Next(1, 7);
+                }
             }
 
-            lblDiceOne.Content = dices[0].ToString();
-            lblDiceTwo.Content = dices[1].ToString();
-            lblDiceThree.Content= dices[2].ToString();
-            lblDiceFour.Content= dices[3].ToString();
-            lblDiceFive.Content = dices[4].ToString();
+            lblDiceOne.Content = _dices[0].ToString();
+            lblDiceTwo.Content = _dices[1].ToString();
+            lblDiceThree.Content = _dices[2].ToString();
+            lblDiceFour.Content = _dices[3].ToString();
+            lblDiceFive.Content = _dices[4].ToString();
 
             // hemläxa
             // använd en for-loop för att summera värdena från alla tärningar
+            int total = 0;
+            for (int i = 0; i < _dices.Length; i++)
+            {
+                // total = total + dices[i]
+                total += _dices[i];
+            }
+
 
         }
+
+        private void btnFives_Click(object sender, RoutedEventArgs e)
+        {
+            // här vill vi satsa på våra femmor
+
+            // skapa om möjligt dina variabler lokalt
+            int scoreForCategory = 0;
+            // Hur gör vi för att samla på någon viss kategori?
+            for (int i = 0; i < _dices.Length; i++)
+            {
+                int diceValue = _dices[i];
+                // jag måste undersöka värdet
+                if (diceValue == 5)
+                {
+                    scoreForCategory += diceValue;
+                }
+            }
+            txtFives.Text = scoreForCategory.ToString();
+            // om det behövs, lägg variabeln i "stora lådan". Instansvariabel
+            _total += scoreForCategory;
+            txtTotal.Text = _total.ToString();
+        }
+
+        private void btnSixes_Click(object sender, RoutedEventArgs e)
+        {
+            // här vill vi satsa på våra sexor
+            int total = 0;
+            // Hur gör vi för att samla på någon viss kategori?
+            for (int i = 0; i < _dices.Length; i++)
+            {
+                int diceValue = _dices[i];
+                // jag måste undersöka värdet
+                if (diceValue == 6)
+                {
+                    total += diceValue;
+                }
+            }
+            txtSixes.Text = total.ToString();
+            _total += total;
+            txtTotal.Text = _total.ToString();
+        }
+
+
+        // hemläxa
+        // Jag vill få veta vilken tärning som är den högsta
+        // och även vilken som är den lägsta
+
+        // svår extrakod
+        // vilka nummer är det som är unika, alltså som förekommer en och endast en gång
     }
 }
